3a78eedb5cef796b755a3ac808d1974c514a5d52
Dane Dexheimer <dane.dexheimer@gmail.com>
2016-07-07 12:37:33 -0400

    maze for clojure

---
+++ src/maze_clojure/core.clj
@@ -0,0 +1,97 @@
+(ns maze-clojure.core
+  (:gen-class))
+
+(def size 10)
+
+(defn create-rooms []
+  (vec
+    (for [row (range size)]
+      (vec
+        (for [col (range size)]
+          {:row row, :col col, :visited? false, :bottom? true, :right? true :start? false})))))
+
+(defn possible-neighbors [rooms row col]
+  (vec
+    (filter
+      (fn [room]
+        (and room (= false (:visited? room))))
+      [(get-in rooms [(dec row) col])
+       (get-in rooms [(inc row) col])
+       (get-in rooms [row (dec col)])
+       (get-in rooms [row (inc col)])])))
+
+(defn random-neighbor [rooms row col]
+  (let [neighbors (possible-neighbors rooms row col)]
+    (if (pos? (count neighbors))
+      (rand-nth neighbors)
+      nil)))
+
+(defn tear-down-wall [rooms old-row old-col new-row new-col]
+  (cond
+    ; going up
+    (< new-row old-row)
+    (assoc-in rooms [new-row new-col :bottom?] false)
+    ; going down
+    (> new-row old-row)
+    (assoc-in rooms [old-row old-col :bottom?] false)
+    ; going left
+    (< new-col old-col)
+    (assoc-in rooms [new-row new-col :right?] false)
+    ; going right
+    (> new-col old-col)
+    (assoc-in rooms [old-row old-col :right?] false)))
+
+(declare create-maze)
+
+(defn create-maze-loop [rooms old-row old-col new-row new-col]
+  (let [new-rooms (tear-down-wall rooms old-row old-col new-row new-col)
+        new-rooms (create-maze new-rooms new-row new-col)]
+    (if (= rooms new-rooms)
+      rooms
+      (create-maze-loop new-rooms old-row old-col new-row new-col))))
+
+(defn create-maze [rooms row col first?]
+  (let [rooms (assoc-in rooms [row col :visited?] true)
+        next-room (random-neighbor rooms row col)
+        rooms (if first?
+                (assoc-in rooms [row col :start?] true) rooms)]
+
+    (if next-room
+      (loop [old-rooms (tear-down-wall rooms row col (:row next-room) (:col next-room))]
+        (let [new-rooms (create-maze old-rooms (:row next-room) (:col next-room) false)]
+          (if (= old-rooms new-rooms)
+            old-rooms
+            (recur new-rooms))))
+      rooms)))
+
+(defn print-room [room]
+  ; changes based on start / bottom
+  (cond
+    (and (:bottom? room) (:start? room)) (print ">")
+    (and (:bottom? room) (not (:start? room))) (print"_")
+    (and (not (:bottom? room)) (:start? room)) (print"v")
+    :else (print" "))
+  ;(if (:bottom? room)
+  ;  (print "_")
+  ;  (print " "))
+  ; gotta always do
+  (if (:right? room)
+    (print "|")
+    (print " ")))
+; if we have bottom and it is a start then ⍜
+; if we have a bottom and it's not a start them " "
+; if we have NO bottom and it is a start then o
+(defn -main []
+  (let [
+        rooms (create-rooms)
+        maze (create-maze rooms 0 0 true)]
+    ; print top walls
+    (doseq [_ maze]
+      (print " _"))
+    (println)
+    ; print grid
+    (doseq [row maze]
+      (print "|")
+      (doseq [room row]
+        (print-room room))
+      (println))))
